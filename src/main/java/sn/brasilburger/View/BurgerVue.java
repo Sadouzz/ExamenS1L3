6a4c797f79b5684ca43576172b0950229790eac8
Sadouzz <ousmansadjo0@gmail.com>
2025-12-09 01:20:17 +0000

    saisieBurger on BurgerVue

---
+++ src/main/java/sn/brasilburger/View/BurgerVue.java
@@ -0,0 +1,31 @@
+package sn.brasilburger.View;
+
+import sn.brasilburger.Entity.Burger;
+import sn.brasilburger.Service.BurgerService;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class BurgerVue extends Vue {
+    private BurgerService service;
+
+    public BurgerVue(BurgerService service) {
+        this.service = service;
+    }
+
+    public Burger saisieBurger(Scanner scanner) {
+        Burger b = new Burger();
+        b.setId(service.numberOfRows() + 1);
+
+        b.setLibelle(saisieChaine(scanner, "Libellé : "));
+        b.setDesc(saisieChaine(scanner, "Description : "));
+        b.setPrix(Double.parseDouble(saisieChaine(scanner, "Prix : ")));
+        b.setImageUrl(saisieChaine(scanner, "URL Image : "));
+        b.setArchived(false);
+        b.setBurgerCategorieId(Integer.parseInt(saisieChaine(scanner, "ID Catégorie : ")));
+
+        return b;
+    }
+
+
+}
