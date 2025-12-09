a411214531c7b7e0c27ee7c769266faa1ee87446
Sadouzz <ousmansadjo0@gmail.com>
2025-12-09 00:26:17 +0000

    NumberOfRows on BurgerCategorieRepository

--- src/main/java/sn/brasilburger/Repository/Impl/BurgerCategorieRepositoryImpl.java
+++ src/main/java/sn/brasilburger/Repository/Impl/BurgerCategorieRepositoryImpl.java
@@ -19,6 +19,29 @@ public class BurgerCategorieRepositoryImpl implements BurgerCategorieRepository 
         this.database = database;
     }
 
+    @Override
+    public int numberOfRows() {
+        int count = 0;
+        try {
+            if (!database.isConnected()) {
+                throw new SQLException("Erreur de connexion à la BD");
+            }
+            Connection conn = database.getConnection();
+            PreparedStatement ps = conn.prepareStatement("SELECT COUNT(*) FROM burger_categorie");
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                count = rs.getInt(1);
+            }
+            rs.close();
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return count;
+    }
+
+
 
 
 }
