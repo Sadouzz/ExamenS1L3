f7f83acb96b20c1cf0c4dddc40e0063b0df685ba
Sadouzz <ousmansadjo0@gmail.com>
2025-12-09 01:06:57 +0000

    createEntity on MenuComplementService

--- src/main/java/sn/brasilburger/Service/MenuComplementService.java
+++ src/main/java/sn/brasilburger/Service/MenuComplementService.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 public interface MenuComplementService {
     void createMenuComplement(MenuComplement menuComplement);
 
+    Optional<MenuComplement> selectById(int id);
+
 
 }
 
