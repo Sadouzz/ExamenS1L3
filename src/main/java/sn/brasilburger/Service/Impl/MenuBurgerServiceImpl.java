cf45c57e374658fe5f3b38615d34b04829942a05
Sadouzz <ousmansadjo0@gmail.com>
2025-12-09 01:03:14 +0000

    Init de MenuBurgerService

---
+++ src/main/java/sn/brasilburger/Service/Impl/MenuBurgerServiceImpl.java
@@ -0,0 +1,19 @@
+package sn.brasilburger.Service.Impl;
+
+import sn.brasilburger.Entity.MenuBurger;
+import sn.brasilburger.Repository.MenuBurgerRepository;
+import sn.brasilburger.Service.MenuBurgerService;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MenuBurgerServiceImpl implements MenuBurgerService {
+
+    private MenuBurgerRepository menuBurgerRepository;
+
+    public MenuBurgerServiceImpl(MenuBurgerRepository menuBurgerRepository) {
+        this.menuBurgerRepository = menuBurgerRepository;
+    }
+
+
+}
---
+++ src/main/java/sn/brasilburger/Service/MenuBurgerService.java
@@ -0,0 +1,14 @@
+package sn.brasilburger.Service;
+
+import sn.brasilburger.Entity.MenuBurger;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MenuBurgerService {
+
+
+
+}
+
+
