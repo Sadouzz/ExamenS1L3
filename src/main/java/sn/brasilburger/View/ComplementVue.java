fea30d8f2be118189776e82c18bc68d216a58a8a
Sadouzz <ousmansadjo0@gmail.com>
2025-12-09 01:21:33 +0000

    afficheComplements on ComplementVue

--- src/main/java/sn/brasilburger/View/ComplementVue.java
+++ src/main/java/sn/brasilburger/View/ComplementVue.java
@@ -31,5 +31,12 @@ public class ComplementVue extends Vue {
         return c;
     }
 
-
+    public void afficheComplements() {
+        List<Complement> liste = service.selectAll();
+        if (liste.isEmpty()) {
+            System.out.println("Aucun complément.");
+        } else {
+            liste.forEach(System.out::println);
+        }
+    }
 }
