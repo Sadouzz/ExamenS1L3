fa63d5f28f24e444cf324b6c40974b7c0fbcd869
Sadouzz <ousmansadjo0@gmail.com>
2025-12-09 01:22:16 +0000

    saisieMenuBurger on MenuBurgerVue

---
+++ src/main/java/sn/brasilburger/View/MenuBurgerVue.java
@@ -0,0 +1,28 @@
+package sn.brasilburger.View;
+
+import sn.brasilburger.Entity.MenuBurger;
+import sn.brasilburger.Service.MenuBurgerService;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class MenuBurgerVue extends Vue {
+    private MenuBurgerService service;
+
+    public MenuBurgerVue(MenuBurgerService service) {
+        this.service = service;
+    }
+
+    public MenuBurger saisieMenuBurger(Scanner scanner) {
+        MenuBurger mb = new MenuBurger();
+        mb.setId(service.numberOfRows() + 1);
+
+        mb.setMenuId(Integer.parseInt(saisieChaine(scanner, "ID Menu : ")));
+        mb.setBurgerId(Integer.parseInt(saisieChaine(scanner, "ID Burger : ")));
+        mb.setQuantite(Integer.parseInt(saisieChaine(scanner, "Quantité : ")));
+
+        return mb;
+    }
+
+
+}
