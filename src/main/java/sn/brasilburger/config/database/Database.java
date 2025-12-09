4dcb0b9202654865e720a0663d3b1a71cc3d2c63
Sadouzz <ousmansadjo0@gmail.com>
2025-12-09 00:13:42 +0000

    Ajout de Config/Database/Database

---
+++ src/main/java/sn/brasilburger/config/database/Database.java
@@ -0,0 +1,11 @@
+package sn.brasilburger.config.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public interface Database {
+
+}
---
+++ src/main/java/sn/brasilburger/config/database/DatabaseImpl.java
@@ -0,0 +1,42 @@
+package sn.brasilburger.config.database;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class DatabaseImpl implements Database {
+    private Connection connection;
+    public static DatabaseImpl instance;
+
+    public static DatabaseImpl getInstance(Map<String, String> config) {
+        if (instance == null)
+            instance = new DatabaseImpl(config);
+        return instance;
+    }
+
+    public static DatabaseImpl getInstance(String driver, String url, String user, String pwd) {
+        if (instance == null)
+            instance = new DatabaseImpl(driver, url, user, pwd);
+        return instance;
+    }
+
+    private DatabaseImpl(Map<String, String> config) {
+        String driver = config.get("driver");
+        String url = config.get("url");
+        String user = config.get("user");
+        String password = config.get("password");
+
+    }
+
+    private DatabaseImpl(String driver, String url, String user, String pwd) {
+
+    }
+
+
+
+
+
+
+}
